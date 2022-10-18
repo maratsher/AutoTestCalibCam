8b13056fcd9037fedfa492b3e9d16ccf4742e5c7
maratsher <99639132+maratsher@users.noreply.github.com>
2022-10-18 20:31:10 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gigachad/Документы/Code/Test
-# Build directory: /home/gigachad/Документы/Code/Test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
